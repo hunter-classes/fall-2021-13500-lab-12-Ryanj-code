36c5fd2c259a12a8acd04c36d720849c306acd58
jiangr0421@gmail.com <ryanjiang@DESKTOP-109BKGG.localdomain>
2021-11-29 12:19:29 -0500

    Added test for task D

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(){
   std::cout << "\n";
 
   std::cout << "Testing goodVibes:\n";
-  std::vector<int> test2 {1,2,-1,3,4,-1,6};
+  std::vector<int> test2 {1, 2, -1, 3, 4, -1, 6};
   std::vector<int> v2 = goodVibes(test2);
   for(int i = 0; i < v2.size(); i++){
     std::cout << v2.at(i) << "\n";
@@ -19,8 +19,8 @@ int main(){
   std::cout << "\n";
 
   std::cout << "Testing gogeta:\n";
-  std::vector<int> goku {1,2,3};
-  std::vector<int> vegeta {4,5};
+  std::vector<int> goku {1, 2, 3};
+  std::vector<int> vegeta {4, 5};
   std::cout << "Before fusion:\n";
   std::cout << "Goku:\n";
   for(int i = 0; i < goku.size(); i++){
@@ -46,5 +46,26 @@ int main(){
   }
 
   std::cout << "\n";
+
+  std::vector<int> t1 {1, 2, 3};
+  std::vector<int> t2 {4, 5};
+  std::cout << "Testing sumPairWise:\n";
+  std::cout << "t1:\n";
+  for(int i = 0; i < t1.size(); i++){
+    std::cout << t1.at(i) << "\n";
+  }
+
+  std::cout << "\nt2:\n";
+  for(int i = 0; i < t2.size(); i++){
+    std::cout << t2.at(i) << "\n";
+  }
+
+  std::cout << "\nSums:\n";
+  std::vector<int> sums = sumPairWise(t1, t2);
+  for(int i = 0; i < sums.size(); i++){
+    std::cout << sums.at(i) << "\n";
+  }
+  std::cout << std::endl;
+
   return 0;
 }
